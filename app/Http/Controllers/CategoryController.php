8f7cca1c77dff31caea3c2e6f76adde4ae1ae663
marzieh-alidadi <79142914+marzieh-alidadi@users.noreply.github.com>
2021-06-15 13:23:54 +0430

    Error Handling API

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,16 +11,20 @@ use App\Http\Controllers\Controller;
 class CategoryController extends Controller
 {
     public function getAll(Request $request){
-        $allsubcat=Subcategory::all();
-        $i=0;
-        foreach ($allsubcat as $subcat) {
-            $resp[$i]=[
-                'id'=>$subcat->id,
-                'subcategory'=>$subcat->Name,
-                'category'=>Category::find($subcat->category_id)->Name
-            ];
-            $i++;
+        try {
+            $allsubcat = Subcategory::all();
+            $i = 0;
+            foreach ($allsubcat as $subcat) {
+                $resp[$i] = [
+                    'id' => $subcat->id,
+                    'subcategory' => $subcat->Name,
+                    'category' => Category::find($subcat->category_id)->Name
+                ];
+                $i++;
+            }
+            return response()->json($resp);
+        } catch(Exception $ex){
+            abort(500, 'Internal Server Error!');
         }
-        return response()->json($resp);
     }
 }
--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -14,116 +14,133 @@ use Illuminate\Support\Str;
 class FileController extends Controller
 {
     public function upload(Request $request){
-        $user=User::where('remember_token', $request->input('token'))->first();
-        $physicalName=Str::random(30);
-        $new_file=[
-            'name'=>$request->input('name'),
-            'physicalName'=>$physicalName,
-            'description'=>$request->input('description'),
-            'price'=>$request->input('price'),
-            'format'=>$request->input('format'),
-            'size'=>$request->input('size'),
-            'author_id'=>$user->id,
-            'subcategory_id'=>$request->input('subcategory_id')
-        ];
+        try {
+            $user = User::where('remember_token', $request->input('token'))->first();
+            $physicalName = Str::random(30);
+            $new_file = [
+                'name' => $request->input('name'),
+                'physicalName' => $physicalName,
+                'description' => $request->input('description'),
+                'price' => $request->input('price'),
+                'format' => $request->input('format'),
+                'size' => $request->input('size'),
+                'author_id' => $user->id,
+                'subcategory_id' => $request->input('subcategory_id')
+            ];
 
-        //dd($new_file);
-        $file=File::create($new_file);
+            //dd($new_file);
+            $file = File::create($new_file);
 
-        $request->file('file')->storeAs('files',$physicalName);
+            $request->file('file')->storeAs('files', $physicalName);
 
-        if ($file instanceof File) {
-            return response()->json([
-                'code'=>200,
-                'status'=>'OK'
-            ]);
-        }
-        else{
-            return response()->json( [
-                'code'=>409,
-                'status'=>'File Upload Error'
-            ]);
+            if ($file instanceof File) {
+                return response()->json([
+                    'code' => 200,
+                    'status' => 'OK'
+                ]);
+            } else {
+                return response()->json([
+                    'code' => 409,
+                    'status' => 'File Upload Error'
+                ]);
+            }
+        } catch(Exception $ex){
+            abort(500, 'Internal Server Error!');
         }
     }
 
     public function getAll(Request $request){
-        $allfile=File::all();
-        $i=0;
-        foreach ($allfile as $file) {
-            $subcategory=Subcategory::where('id', $file->subcategory_id)->first();
-            $resp[$i]=[
-                'id'=>$file->id,
-                'name'=> $file->name,
-                'price'=> $file->price
-            ];
-            $i++;
+        try {
+            $allfile = File::all();
+            $i = 0;
+            foreach ($allfile as $file) {
+                $subcategory = Subcategory::where('id', $file->subcategory_id)->first();
+                $resp[$i] = [
+                    'id' => $file->id,
+                    'name' => $file->name,
+                    'price' => $file->price
+                ];
+                $i++;
+            }
+            return response()->json($resp);
+        } catch(Exception $ex){
+            abort(500, 'Internal Server Error!');
         }
-        return response()->json($resp);
     }
 
     public function getDetail(Request $request){
-        $file=File::where('id', $request->input('id'))->first();
-        $subcategory=Subcategory::where('id', $file->subcategory_id)->first();
-        $resp=[
-            'id'=>$file->id,
-            'name'=> $file->name,
-            'description'=> $file->description,
-            'price'=> $file->price,
-            'format'=> $file->format,
-            'size'=> $file->size,
-            'author_name'=> User::find($file->author_id)->name.' '.User::find($file->author_id)->family,
-            'author_email'=> User::find($file->author_id)->email,
-            'subcategory'=>Subcategory::find($file->subcategory_id)->Name,
-            'category'=>Category::find($subcategory->category_id)->Name
-        ];
-        return response()->json($resp);
+        try {
+            $file = File::where('id', $request->input('id'))->first();
+            $subcategory = Subcategory::where('id', $file->subcategory_id)->first();
+            $resp = [
+                'id' => $file->id,
+                'name' => $file->name,
+                'description' => $file->description,
+                'price' => $file->price,
+                'format' => $file->format,
+                'size' => $file->size,
+                'author_name' => User::find($file->author_id)->name . ' ' . User::find($file->author_id)->family,
+                'author_email' => User::find($file->author_id)->email,
+                'subcategory' => Subcategory::find($file->subcategory_id)->Name,
+                'category' => Category::find($subcategory->category_id)->Name
+            ];
+            return response()->json($resp);
+        } catch(Exception $ex){
+            abort(500, 'Internal Server Error!');
+        }
     }
 
     public function register(Request $request){
-        $user=User::where('remember_token', $request->input('token'))->first();
-        $file=File::where('id',$request->input('file_id'))->first();
-        $author=User::where('id',$file->author_id)->first();
-        if($file->price>$user->wallet){
+        try {
+            $user = User::where('remember_token', $request->input('token'))->first();
+            $file = File::where('id', $request->input('file_id'))->first();
+            $author = User::where('id', $file->author_id)->first();
+            if ($file->price > $user->wallet) {
+                return response()->json([
+                    'code' => 400,
+                    'status' => 'Not enough money'
+                ]);
+            }
+            $newfileRegister = [
+                'file_id' => $file->id,
+                'user_id' => $user->id,
+                'price' => $file->price,
+            ];
+
+            $user->wallet = $user->wallet - $file->price;
+            $author->wallet = $author->wallet + $file->price;
+            $user->save();
+            $author->save();
+
+            FileRegister::create($newfileRegister);
             return response()->json([
-                'code'=>400,
-                'status'=>'Not enough money'
+                'code' => 200,
+                'status' => 'OK'
             ]);
+        } catch(Exception $ex){
+            abort(500, 'Internal Server Error!');
         }
-        $newfileRegister=[
-            'file_id'=>$file->id,
-            'user_id'=>$user->id,
-            'price'=>$file->price,
-        ];
-
-        $user->wallet=$user->wallet-$file->price;
-        $author->wallet=$author->wallet+$file->price;
-        $user->save();
-        $author->save();
-
-        FileRegister::create($newfileRegister);
-        return response()->json([
-            'code'=>200,
-            'status'=>'OK'
-        ]);
-
     }
 
     public function checkRegister(Request $request){
-        $user=User::where('remember_token', $request->input('token'))->first();
-        $fileRegister=FileRegister::where('user_id',$user->id)->where('file_id',$request->input('file_id'))->first();
-        if ($fileRegister instanceof FileRegister) {
-            return response()->json([
-                'result'=>'true',
-                'code'=>200,
-                'status'=>'OK'
-            ]);
-        }
-        else{
-            return response()->json([
-                'result'=>'false',
-                'code'=>200,
-                'status'=>'OK'
-            ]);
+        try {
+            $user = User::where('remember_token', $request->input('token'))->first();
+            $fileRegister = FileRegister::where('user_id', $user->id)->where('file_id', $request->input('file_id'))->first();
+            if ($fileRegister instanceof FileRegister) {
+                return response()->json([
+                    'result' => 'true',
+                    'code' => 200,
+                    'status' => 'OK'
+                ]);
+            } else {
+                return response()->json([
+                    'result' => 'false',
+                    'code' => 200,
+                    'status' => 'OK'
+                ]);
+            }
+        } catch(Exception $ex){
+            abort(500, 'Internal Server Error!');
         }
     }
 }
